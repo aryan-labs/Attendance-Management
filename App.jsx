e6c7402da71b22f79a55151185b298469ab8f666
Monal-Jain01 <monaljain1901@gmail.com>
2025-03-25 21:16:45 +0530

    Add files via upload
    Add MarkAttendance component

---
+++ App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './index.css'
+import Home from './pages/Home'
+import { Route, Router, Routes } from 'react-router-dom'
+import Register from './components/Register'
+import Login from './components/Login'
+import MarkAttendance from './components/MarkAttendance'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home></Home>}/>
+        <Route path='/Login' element={<Login></Login>}/>
+        <Route path='/Register' element={<Register></Register>}/>
+        <Route path='/MarkAttendance' element={
+          <MarkAttendance
+            subjects={['English', 'Maths', 'Physics','Chemistry','Engineering Drawing']}
+          />
+        }>
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
