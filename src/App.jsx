5ba2f032f903bfba72a50c22b8cb3d0c634df9af
Monal-Jain01 <monaljain1901@gmail.com>
2025-03-25 21:23:26 +0530

    Update App.jsx
    Add MarkAttendance

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,22 @@ import Home from './pages/Home'
 import { Route, Router, Routes } from 'react-router-dom'
 import Register from './components/Register'
 import Login from './components/Login'
+import MarkAttendance from './components/MarkAttendance'
 
 const App = () => {
   return (
     <div>
-   <Routes>
-      <Route path='/' element={<Home></Home>}/>
-      <Route path='/Login' element={<Login></Login>}/>
-      <Route path='/Register' element={<Register></Register>}/>
+      <Routes>
+        <Route path='/' element={<Home></Home>}/>
+        <Route path='/Login' element={<Login></Login>}/>
+        <Route path='/Register' element={<Register></Register>}/>
+        <Route path='/MarkAttendance' element={
+          <MarkAttendance
+            subjects={['English', 'Maths', 'Physics','Chemistry','Engineering Drawing']}
+          />
+        }>
+        </Route>
       </Routes>
-      
-
     </div>
   )
 }
