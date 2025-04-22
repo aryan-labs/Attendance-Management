7f1d7472bc361d18efc60c6befb6e156d0592f26
ambikagoswami <ambikagoswami02849@gmail.com>
2025-04-22 18:20:08 +0530

    Updated App.jsx , Reports.jsx , MyProfile.jsx

--- App.jsx
+++ App.jsx
@@ -1,38 +1,15 @@
-import React from 'react'
-import './index.css'
-import Home from './components/Home'
+import React from "react";
+import Reports from './components/Reports';
+import MyProfile from "'./components/MyProfile'";
 import { Route, Router, Routes } from 'react-router-dom'
-import Register from './components/Register'
-import Login from './components/Login'
-import MarkAttendance from './components/MarkAttendance'
-import Profile from './components/Profile'
-import SideBar from './components/SideBar'
-import Settings from './components/Settings'
-import Dashboard from './components/Dashboard'
-import Header from './components/Header'
 
-const App = () => {
-  return (
+function App(){
+  return(
     <div>
-      <Header />
-    <div className='flex flex-row gap-0'>
-      <SideBar />
-      <Routes>
-        <Route path='/' element={<Home></Home>}/>
-        <Route path='/SideBar' element={<SideBar></SideBar>}/>
-        <Route path='/Login' element={<Login></Login>}/>
-        <Route path='/Profile' element={<Profile></Profile>}/>
-        <Route path='/Register' element={<Register></Register>}/>
-        <Route path='/Dashboard' element={<Dashboard></Dashboard>}/>
-        <Route path='/Settings' element={<Settings></Settings>}/>
-        <Route path='/MarkAttendance' element={
-          <MarkAttendance />
-        }>
-        </Route>
-      </Routes>
+      <Reports />
+      <MyProfile/>
     </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
